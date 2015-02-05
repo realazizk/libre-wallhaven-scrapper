16533a128506fdaa3d89bc1ac7c589a23fe9e563
mohamed-aziz <medazizknani@gmail.com>
2015-02-05 22:23:06 +0000

    added GUI

--- randomWall.py
+++ randomWall.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python2
 
-PATH = '/home/mohamed/wallpapers'
-PAGES = 5
+import Tkinter
+
+#PATH = '/home/mohamed/wallpapers'
+#PAGES = 5
 
 def screenRes() :
-	import Tkinter
 	return { 'height' : Tkinter.Tk().winfo_screenheight(),
 	         'width' : Tkinter.Tk().winfo_screenwidth() }
 
 def save(url, filename) :
 	import urllib
 	from os import path
-	sv = path.join(PATH, filename + '.' + 'jpg')
+	sv = path.join(pt.get(), filename + '.' + 'jpg')
 	urllib.urlretrieve(url, sv)
 
 def crawl(height, width, page) :
@@ -19,7 +20,7 @@ def crawl(height, width, page) :
 	from bs4 import BeautifulSoup
 	import re
 	url = 'http://alpha.wallhaven.cc/search?categories=111&purity=100&resolutions=%sx%s&sorting=random&order=desc&page=%s' % (width, height, page)
-	print url
+	#print url
 	html = urllib2.urlopen(url)
 	soup = BeautifulSoup(html, 'html')
 	ss = soup.findAll('a', {'class' : 'preview' })
@@ -29,6 +30,22 @@ def crawl(height, width, page) :
 		#print url
 		save(url, filename)
 
-for i in range(1, PAGES+1) :
-	dic = screenRes()
-	crawl(dic['height'], dic['width'], i)
+def doit() :
+	for i in range(1, int(pg.get())+1) :
+		dic = screenRes()
+		crawl(dic['height'], dic['width'], i)
+
+root = Tkinter.Tk()
+root.wm_title('randomWall')
+
+Tkinter.Label(root, text="PATH").grid(row=0)
+pt = Tkinter.Entry(root)
+pt.insert(0, '/home/mohamed/wallpapers')
+pt.grid(row=0, column=1)
+
+Tkinter.Label(root, text="PAGES").grid(row=1)
+pg = Tkinter.Entry(root)
+pg.grid(row=1, column=1)
+
+Tkinter.Button(root, text='GO !', command=doit).grid(row=3, columnspan=2)
+root.mainloop()
